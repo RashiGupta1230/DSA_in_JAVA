55c664ad64de3205b4d0e7fb20c0e4b414f06945
RashiGupta1230 <cuterashigupta30@gmail.com>
2026-02-12 11:44:34 +0530

    Added TimeToEqualTheElements.java

---
+++ src/TechnicalSkillsLecture/UpperDiagonal.java
@@ -0,0 +1,22 @@
+package TechnicalSkillsLecture;
+
+public class UpperDiagonal {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        int sum = 0;
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr.length;j++){
+                if(i<j){
+                    System.out.println(arr[i][j]);
+                    sum += arr[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
