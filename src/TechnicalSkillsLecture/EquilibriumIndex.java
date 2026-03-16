96eebdb29dbb897e1ac4a492c84e43def1a4ee8a
RashiGupta1230 <cuterashigupta30@gmail.com>
2026-03-16 14:51:59 +0530

    Added TimeToEqualTheElements.java

--- src/TechnicalSkillsLecture/EquilibriumIndex.java
+++ src/TechnicalSkillsLecture/EquilibriumIndex.java
@@ -1,6 +1,6 @@
 package TechnicalSkillsLecture;
 public class EquilibriumIndex {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int arr[] = {1, 3, 5, 2, 2};
         int total = 0;
         for(int i = 0; i < arr.length; i++)
