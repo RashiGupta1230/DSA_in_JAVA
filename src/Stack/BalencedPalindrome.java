1480f9d96eb6ba39346b7329f8a77bbaa2a514ff
RashiGupta1230 <cuterashigupta30@gmail.com>
2026-03-19 11:37:20 +0530

    Added TimeToEqualTheElements.java

--- src/Stack/BalencedPalindrome.java
+++ src/Stack/BalencedPalindrome.java
@@ -16,21 +16,10 @@ public class BalencedPalindrome {
             return st.isEmpty();
         }
 
-        static boolean isPalindrome(String s) {
-            int i = 0, j = s.length() - 1;
-            while (i < j) {
-                if (s.charAt(i) != s.charAt(j)) return false;
-                i++;
-                j--;
-            }
-            return true;
-        }
-
         public static void main(String[] args) {
             String s1 = "{[()]}";
-            String s2 = "madam";
             System.out.println(isBalanced(s1));
-            System.out.println(isPalindrome(s2));
+
         }
     }
 
