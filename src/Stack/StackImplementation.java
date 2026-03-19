609a6c0747af27e7e80fbfda27b408efb5148f3b
RashiGupta1230 <cuterashigupta30@gmail.com>
2026-03-19 15:37:00 +0530

    Added TimeToEqualTheElements.java

--- src/Stack/StackImplementation.java
+++ src/Stack/StackImplementation.java
@@ -1,5 +1,7 @@
 package Stack;
 
+import java.util.Scanner;
+
 public class StackImplementation {
     static int stack[] = new int[5];
     static int top = -1;
@@ -8,10 +10,10 @@ public class StackImplementation {
         if (top == 4) {
             System.out.println("Stack Full");
         } else {
-            top++;
-            stack[top] = x;
+            stack[++top] = x;
         }
     }
+
     static void peek() {
         if (top == -1) {
             System.out.println("Stack Empty");
@@ -20,29 +22,60 @@ public class StackImplementation {
         }
     }
 
-    static void pop(){
-        if (top==-1){
-            System.out.println("Stack Empty");
-        }
-        else{
-            System.out.println("Top Element : "+stack[top]);
-        }
-    }
-    static void display(){
-        if(top==-1) {
+    static void pop() {
+        if (top == -1) {
             System.out.println("Stack Empty");
         } else {
-            for(int i =top;i>=0;i--){
+            System.out.println("Popped: " + stack[top--]);
+        }
+    }
+
+    static void isEmpty() {
+        if (top == -1)
+            System.out.println("Stack Empty");
+        else
+            System.out.println("Stack Not Empty");
+    }
+
+    static void display() {
+        if (top == -1) {
+            System.out.println("Stack Empty");
+        } else {
+            for (int i = top; i >= 0; i--) {
                 System.out.println(stack[i]);
             }
         }
-    }public static void main (String args[]){
-        push(10);
-        push(20);
-        push(30);
-        display();
-        pop();
-        peek();
     }
-        }
 
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n1.Push 2.Pop 3.Peek 4.isEmpty 5.Display 6.Exit");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter element: ");
+                    int x = sc.nextInt();
+                    push(x);
+                    break;
+                case 2:
+                    pop();
+                    break;
+                case 3:
+                    peek();
+                    break;
+                case 4:
+                    isEmpty();
+                    break;
+                case 5:
+                    display();
+                    break;
+            }
+        } while (choice != 6);
+
+        sc.close();
+    }
+}
